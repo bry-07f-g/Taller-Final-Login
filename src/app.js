abc7228b0d65505c88472de676bc5086bc6d22e6
Bryan Franco Gomez <francobryan524@gmail.com>
2026-04-08 15:42:38 -0500

    fix: cors para vercel

--- src/app.js
+++ src/app.js
@@ -2,20 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
-
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
+// ✅ CORS para local + Vercel
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: [
+    'http://localhost:5173',
+    'https://taller-final-gamma.vercel.app'
+  ]
 }));
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+// rutas
 app.use('/api/auth', authRoutes);
 
+// ruta de prueba
 app.get('/', (req, res) => {
   res.send('API funcionando 🚀');
 });
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
-import app from './app.js'
+import app from './app.js';
 
-const { MONGODB_URI, PORT } = process.env
+const { MONGODB_URI } = process.env;
+const PORT = process.env.PORT || 4000;
 
+// conexión a MongoDB
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Conectado a MongoDB Atlas');
+
         app.listen(PORT, () => {
-            console.log(`Servidor escuchado en https://localhost:${PORT}`)
+            console.log(`Servidor corriendo en puerto ${PORT}`);
         });
     })
     .catch((err) => {
         console.error('Error conectando a MongoDB:', err.message);
         process.exit(1);
-    })
+    });
